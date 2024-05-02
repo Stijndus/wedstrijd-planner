ee099395030f11bc76d80700a4834b611b3c822a
Stijn Dusseldorp | Humble IT <stijn@humble-it.com>
2024-05-02 15:21:10 +0200

    technical: init project

---
+++ Wedstijd-planner/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Wedstijd_planner
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Wedstijd-planner/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Wedstijd_planner
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
